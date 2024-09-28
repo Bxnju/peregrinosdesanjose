11b2dd397fda7fbcbec012553fdc3ce115683c62
Andrés Benjumea Reinoso <97989812+Bxnju@users.noreply.github.com>
2024-09-28 13:00:02 -0500

    File keys have been re-added for proper deployment operation

--- js/querys/loadIntentions.js
+++ js/querys/loadIntentions.js
@@ -1,5 +1,5 @@
-const spreadsheetId = 'id_sheet';
-const sheetName = 'intenciones';
-const apiKey = 'id_api';
+const spreadsheetId = '1MfQjpDdIEQdx0g_PXjAHiwUKaYtVmM-WwOhwUlVoAZU';
+const sheetName = 'Respuestas_pruebas';
+const apiKey = 'AIzaSyAnKtoEapc41RnXlfDeM-jgoOm4AMrCWsk';
 
 export const apiUrl = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${sheetName}?key=${apiKey}`;
